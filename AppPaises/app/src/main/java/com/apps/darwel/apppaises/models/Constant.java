07ccfecd4c4dc498bd26723264b6845fbf16d2f2
Darwel Quintero <darwel34@gmail.com>
2017-01-09 17:06:37 -0430

    se agrego el nombre de la variable en que comienza la paginacion2

--- AppPaises/app/src/main/java/com/apps/darwel/apppaises/models/Constant.java
+++ AppPaises/app/src/main/java/com/apps/darwel/apppaises/models/Constant.java
@@ -4,4 +4,5 @@ package com.apps.darwel.apppaises.models;
  * Created by Darwel on 05/01/2017.
  */
 public class Constant {
+    public static final String STARTFROM_EXTRA = "com.apps.darwel.apppaises.StartFrom";
 }
